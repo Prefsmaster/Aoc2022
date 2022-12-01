a8ff06e793e2acdedda88468c5df69c4a377f0df
Ferenc <ferencn@gmail.com>
2022-12-01 16:56:02 +0100

    fixed bug,

--- Aoc202201/Program.cs
+++ Aoc202201/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var values = File.ReadAllLines(@"input.txt");
+var values = File.ReadAllLines(@"Test.txt");
 
 var caloriesOfElf = 0L;
 var totalCaloriesPerElf = new List<long>();
@@ -17,5 +17,8 @@ foreach (var value in values)
         caloriesOfElf += long.Parse(value);
     }
 }
+if (caloriesOfElf!=0) 
+    totalCaloriesPerElf.Add(caloriesOfElf);
+
 Console.WriteLine($"Largest 1: {totalCaloriesPerElf.Max()}");
 Console.WriteLine($"Largest 3: {totalCaloriesPerElf.OrderByDescending(x => x).Take(3).Sum()}");
