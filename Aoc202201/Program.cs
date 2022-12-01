7bff0c1aa1c0e8be42c61ef9e6ec53c9317c1b3a
Ferenc <ferencn@gmail.com>
2022-12-01 16:30:52 +0100

    Add project files.

---
+++ Aoc202201/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var values = File.ReadAllLines(@"input.txt").ToList();
+
+var caloriesOfElf = 0L;
+var totalCaloriesPerElf = new List<long>();
+
+foreach (var value in values)
+{
+    if (string.IsNullOrEmpty(value))
+    {
+        totalCaloriesPerElf.Add(caloriesOfElf);
+        caloriesOfElf = 0;
+    }
+    else
+    {
+        caloriesOfElf += int.Parse(value);
+    }
+}
+Console.WriteLine($"Largest 1: {totalCaloriesPerElf.MaxBy(x => x)}");
+Console.WriteLine($"Largest 3: {totalCaloriesPerElf.OrderByDescending(x => x).Take(3).Sum()}");
