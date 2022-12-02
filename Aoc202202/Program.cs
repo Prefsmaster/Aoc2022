fa2b7e5e10ff4326fad933686091a85dcb12c510
Ferenc <ferencn@gmail.com>
2022-12-02 16:37:56 +0100

    comment typo fixed

--- Aoc202202/Program.cs
+++ Aoc202202/Program.cs
@@ -31,7 +31,7 @@ foreach (var game in instructions)
     // first transform to zero based index abc -> 012 xyz->012: 0 2
     // then add these = 2. then add another 2 = 4. 
     // take remainder of division by 3 = 1
-    // result: Opponents rock (A=0) must be beaten (Z) by paper (B=2)
+    // result: Opponents rock (A=0) must be beaten (Z) by paper (B=1)
     // this works in all cases :-)
     var iMustPick = (opponent + me + 2) % 3;
     score2 += iMustPick + 1;
