f76ca50fc35b04b5af2df5ea183bf07e1ccd609a
Ferenc <ferencn@gmail.com>
2022-12-05 17:38:07 +0100

    Single! always only 1 common item.

--- Aoc202203/Program.cs
+++ Aoc202203/Program.cs
@@ -1,13 +1,15 @@
 ﻿var contents = File.ReadAllLines(@"input.txt");
 
-Console.WriteLine(contents.Select(line => line[..(line.Length / 2)]
-        .Intersect(line[(line.Length / 2)..]))
-    .Select(common => common.Sum(item => (item > 'Z') ? item - 'a' + 1 : item - 'A' + 27))
-    .Sum());
+Console.WriteLine (
+        contents.Select (line => line[..(line.Length / 2)].Intersect(line[(line.Length / 2)..]).Single())
+                .Select(common => (common > 'Z') ? common - 'a' + 1 : common - 'A' + 27)
+                .Sum()
+        );
 
-Console.WriteLine(contents.Chunk(3)
-    .Select(triplet => triplet[0].Intersect(triplet[1].Intersect(triplet[2]))
-        .First())
-    .Select(common => (common > 'Z') ? common - 'a' + 1 : common - 'A' + 27)
-    .Sum());
+Console.WriteLine (
+     contents.Chunk(3)
+             .Select(triplet => triplet[0].Intersect(triplet[1].Intersect(triplet[2])).Single())
+             .Select(common => (common > 'Z') ? common - 'a' + 1 : common - 'A' + 27)
+             .Sum()
+     );
 
