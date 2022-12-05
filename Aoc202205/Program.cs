4128db1c72b62ed7d920478a205ec1a77d101b1c
Ferenc <ferencn@gmail.com>
2022-12-06 00:23:28 +0100

    refactored initialization of arraylists to functional approach

--- Aoc202205/Program.cs
+++ Aoc202205/Program.cs
@@ -2,16 +2,11 @@
 
 var stacks = PrepStacks(file); // returns stacks in wrong(reversed) order!
 // reverse and make 2 copies
-var stacks1 = new List<Stack<char>>();
-var stacks2 = new List<Stack<char>>();
-foreach (var s in stacks)
-{
-    stacks1.Add(new Stack<char>(s)); // initializing with a stack reverses order 
-    stacks2.Add(new Stack<char>(s)); // because it uses pop source-push destination
-}
-// skip empty line
-file.ReadLine();
-// process instructions
+var stacks1 = stacks.Select(s => new Stack<char>(s)).ToList();
+var stacks2 = stacks.Select(s => new Stack<char>(s)).ToList();
+
+file.ReadLine(); // skip empty line
+
 string? instruction;
 while (!string.IsNullOrEmpty(instruction = file.ReadLine())) 
 {
@@ -34,24 +29,21 @@ Console.WriteLine();
 stacks2.ForEach(c => Console.Write(c.Peek()));
 Console.WriteLine();
 
-static Stack<char>[] PrepStacks(StreamReader file)
+static List<Stack<char>> PrepStacks(StreamReader file)
 {
-    var stackList = new List<Stack<char>>();
     var data = file.ReadLine();
     var numberOfStacks = data.Length/4+1;
-    for (var i=0;i< numberOfStacks; i++)
-        stackList.Add(new Stack<char>());
+    var stackList = Enumerable.Range(0, numberOfStacks).Select(i => new Stack<char>()).ToList();
 
-    do
+    while (data[1] != '1')
     {
         for (var i = 0; i < numberOfStacks; i++)
         {
             if (data[i * 4 + 1] != ' ')
                 stackList[i].Push(data[i * 4 + 1]);
         }
-
         data = file.ReadLine();
-    } while (data[1] != '1');
-    return stackList.ToArray();
+    } 
+    return stackList;
 }
 
