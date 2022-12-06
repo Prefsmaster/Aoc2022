7f59e1c9f19a657f366e2929aa00f227b8d27a43
Ferenc <ferencn@gmail.com>
2022-12-06 10:01:47 +0100

    Day 6 done

---
+++ Aoc202206/Program.cs
@@ -0,0 +1,13 @@
+﻿var streams = File.ReadAllLines(@"input.txt");
+foreach (var stream in streams)
+{
+    Console.WriteLine($"{FindMarker(stream, 4)}");
+    Console.WriteLine($"{FindMarker(stream, 14)}");
+}
+
+static int FindMarker(string str, int len)
+{
+    var end = len;
+    while (end <= str.Length && str[(end - len)..end].Distinct().Count() != len) end++;
+    return end;
+}
