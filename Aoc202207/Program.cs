b34d1d5334d8032fbf29802275d76cfbd14f9f9a
Ferenc <ferencn@gmail.com>
2022-12-07 17:46:17 +0100

    better names

--- Aoc202207/Program.cs
+++ Aoc202207/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Aoc202207;
 
-var dirTree = ProcessCommands(File.ReadAllLines(@"input.txt"));
+var dirTree = BuildDirectoryTreeFromConsoleLog(File.ReadAllLines(@"input.txt"));
 
 Console.WriteLine(dirTree.FilterDirectories(item => item.Size <= 100_000).Sum(c => c.Size));
 
@@ -8,26 +8,26 @@ Console.WriteLine(dirTree.FilterDirectories(item => item.Size <= 100_000).Sum(c 
 // this can be simplified to: size of root - 40000000
 Console.WriteLine(dirTree.FilterDirectories(item => item.Size >= dirTree.Size - 40000000L).Min(c => c.Size));
 
-static DirItem ProcessCommands(string[] commands)
+static DirItem BuildDirectoryTreeFromConsoleLog(string[] commands)
 {
     var root = new DirItem(null, "/");
-    DirItem? currentdir = root;
+    var currentDir = root;
     foreach(var command in commands)
     {
         if (command[0]=='$')
         {
             if (command[2] != 'c') continue; // only cd does something, rest can be skipped
-            currentdir = command[5..] switch
+            currentDir = command[5..] switch
             {
                 "/" => root,
-                ".." => currentdir?.Parent,
-                _ => currentdir?.Items.Single(i => i.Name == command[5..])
+                ".." => currentDir?.Parent,
+                _ => currentDir?.Items.Single(i => i.Name == command[5..])
             };
         }
         else // add new item (dir or file) to list
         {
             var parts = command.Split(' ');
-            currentdir?.Items.Add(new DirItem(currentdir, parts[1], command[0] == 'd' ? -1: long.Parse(parts[0])));
+            currentDir?.Items.Add(new DirItem(currentDir, parts[1], command[0] == 'd' ? -1: long.Parse(parts[0])));
         }
     }
     return root;
