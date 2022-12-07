b42bf6df5016fcfad3bdda08e202ea9977c5611c
Ferenc <ferencn@gmail.com>
2022-12-07 17:50:15 +0100

    made recursive method static

--- Aoc202207/DirItem.cs
+++ Aoc202207/DirItem.cs
@@ -2,10 +2,10 @@
 {
     internal class DirItem
     {
-        public DirItem? Parent;
-        public List<DirItem> Items = new();
+        public DirItem? Parent { get; set; }
+        public List<DirItem> Items { get; set; } = new();
         public string Name { get; set; }
-        private  long _size;
+        private readonly long _size;
         public bool IsDir => _size == -1; 
         public DirItem(DirItem? parent, string name, long size = -1)
         {
@@ -21,12 +21,12 @@
             FilterItems(filter, this, filtered);
             return filtered;
         }
-        private void FilterItems(Func<DirItem, bool> filter, DirItem item, List<DirItem> candidates)
+        private static void FilterItems(Func<DirItem, bool> filter, DirItem item, List<DirItem> candidates)
         {
-            foreach (var subitem in item.Items.Where(i => i.IsDir))
+            foreach (var subItem in item.Items.Where(i => i.IsDir))
             {
-                if (filter(subitem)) candidates.Add(subitem);
-                FilterItems(filter, subitem, candidates);
+                if (filter(subItem)) candidates.Add(subItem);
+                FilterItems(filter, subItem, candidates);
             }
         }
     }
