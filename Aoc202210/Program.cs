9c6cf15ca86d4d8f230a20ef7cf59aad1aab7cd1
Ferenc <ferencn@gmail.com>
2022-12-10 16:56:38 +0100

    Day 10 solved

---
+++ Aoc202210/Program.cs
@@ -0,0 +1,41 @@
+﻿var puzzleInput = File.ReadAllLines(@"input.txt");
+
+var RegisterX = 1;
+var CPUcycle = 1;
+var CRTcycle = 0;
+
+long answer = 0;
+
+foreach (var instruction in puzzleInput)
+{
+    if (instruction[0] == 'n')
+    {
+        UpdateAnswer(++CPUcycle);
+        UpdateCrt(CRTcycle++);
+    }
+    else
+    {
+        UpdateAnswer(++CPUcycle);
+        UpdateCrt(CRTcycle++);
+        UpdateCrt(CRTcycle++);
+        RegisterX += int.Parse(instruction[5..]);
+        UpdateAnswer(++CPUcycle);
+    }
+}
+Console.WriteLine(answer);
+
+void UpdateAnswer(int cycleCounter)
+{
+    if (cycleCounter is >= 20 and <= 220 && ((cycleCounter-20) % 40 == 0))
+    {
+        answer += RegisterX * cycleCounter;
+    }
+}
+
+void UpdateCrt(int cycleCounter)
+{
+    var pos = cycleCounter % 40;
+    Console.Write(pos>=RegisterX-1 && pos<=RegisterX+1 ? "#" : ".");
+    if (pos == 39) Console.WriteLine();
+}
+
