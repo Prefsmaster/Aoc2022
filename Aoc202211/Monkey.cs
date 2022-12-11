ba4823a2a9882c2132eb3c78d2098a28d8a155bb
Ferenc <ferencn@gmail.com>
2022-12-11 17:06:43 +0100

    Day 11 Solved!

---
+++ Aoc202211/Monkey.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace Aoc202211;
+
+public class Monkey
+{
+    private List<ulong> Items { get; set; }
+    private char Operator { get; set; }
+    private uint Argument { get; set; }
+    public ulong Divisor;
+    private int MoveToWhenTrue;
+    private int MoveToWhenFalse;
+    public ulong ItemsInspected;
+
+    public Monkey(string[] inputData)
+    {
+        Items = inputData[1][18..].Split(',').Select(ulong.Parse).ToList();
+        Operator = inputData[2][23];
+        if (inputData[2][25] == 'o') Operator = '2';
+        else Argument = uint.Parse(inputData[2][24..]);
+        Divisor = uint.Parse(inputData[3][21..]);
+        MoveToWhenTrue = int.Parse(inputData[4][29..]);
+        MoveToWhenFalse = int.Parse(inputData[5][30..]);
+    }
+
+    public void PlayRound(Monkey[] monkeys, ulong modulo = 0)
+    {
+        while (Items.Any())
+        {
+            ItemsInspected++;
+            var item = Items[0];
+            Items.RemoveAt(0);
+            item = Operator switch
+            {
+                '*' => item * Argument,
+                '+' => item + Argument,
+                '2' => item * item,
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            if (modulo == 0) item /= 3;
+            else item %= modulo;
+            monkeys[item % Divisor == 0?MoveToWhenTrue: MoveToWhenFalse].Items.Add(item);
+        } 
+    }
+}
---
+++ Aoc202211/Program.cs
@@ -0,0 +1,19 @@
+﻿using Aoc202211;
+
+var puzzleInput = File.ReadAllLines(@"input.txt");
+var monkeys = puzzleInput.Chunk(7).Select(c=>new Monkey(c)).ToArray();
+
+SolveDay11(20);
+SolveDay11(10000, true);
+
+void SolveDay11(int roundstoplay, bool useModulo = false)
+{
+    foreach (var monkey in monkeys) monkey.ItemsInspected = 0;
+
+    var modulo = useModulo? monkeys.Select(m => m.Divisor).Aggregate((x, y) => x * y) : 0L;
+    
+    while (roundstoplay-- > 0) foreach (var m in monkeys) m.PlayRound(monkeys, modulo);
+
+    var MonkeyBusiness = monkeys.Select(m => m.ItemsInspected).OrderDescending().Take(2).Aggregate((x, y) => x * y);
+    Console.WriteLine(MonkeyBusiness);
+}
